887581e337b9621bf7b540474e0ef2b5ecfa46f6
Andrii Popov <popov.andrey80@gmail.com>
2022-09-18 22:39:56 +0300

    task-08 done!

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const inputData = event.currentTarget.elements;
+
+  if (inputData.email.value === '' || inputData.password.value === '') {
+    alert('Please, fill in all fields the form!');
+  }
+
+  const formData = {
+    email: inputData.email.value,
+    password: inputData.password.value,
+  };
+
+  console.log(formData);
+
+  //   const formData = new FormData(event.currentTarget);
+
+  //   formData.forEach((value, name) => {
+  //     console.log('onFormSubmit -> name', name);
+  //     console.log('onFormSubmit -> value', value);
+  //   });
+
+  event.currentTarget.reset();
+}
