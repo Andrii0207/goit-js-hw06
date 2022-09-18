c57e64c79afa5eb527b856ad68c3fd2c0ba3f2a4
Andrii Popov <popov.andrey80@gmail.com>
2022-09-18 23:48:19 +0300

    task-09 done!

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  changeColorBtn: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+};
+
+refs.changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+  refs.color.textContent = getRandomHexColor();
+}
