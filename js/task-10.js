4e66ffab3c903c3bd6a6245bbd99d5b005141aa5
Andrii Popov <popov.andrey80@gmail.com>
2022-09-19 00:38:50 +0300

    start task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  create: document.querySelector('[data-create]'),
+  destroy: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+};
+
+refs.create.addEventListener('click', createBoxes);
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+  console.dir(refs.input.value);
+
+  refs.boxes.createElement('<div></div>');
+}
